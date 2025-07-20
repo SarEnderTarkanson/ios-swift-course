2a23b2f8712fca0ac122f5fb9feeba854aba9dcc
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-20 03:36:51 +0200

    Initial commit

---
+++ LOTRConverter/LOTRConverter/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  LOTRConverter
+//
+//  Created by Alpy on 20/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .padding()
+            Text("Hello, world!")
+                .foregroundColor(Color.green)
+            Button("Button"){
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter/LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Alpy on 20/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
