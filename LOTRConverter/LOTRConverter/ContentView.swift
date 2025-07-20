225ff132997df142742f6be66171f70a5eafdb90
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-20 04:01:15 +0200

    laying out our main screen with stacks

--- LOTRConverter/LOTRConverter/ContentView.swift
+++ LOTRConverter/LOTRConverter/ContentView.swift
@@ -9,18 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-                .padding()
-            Text("Hello, world!")
-                .foregroundColor(Color.green)
-            Button("Button"){
-                
+        ZStack{
+            // Background Image
+            VStack{
+                {
+                    //Prancing Pony ImageView
+                    //Curency exchange text
+                    //Conversion Section
+                    HStack{
+                        //Left conversion section
+                        VStack{
+                        //Currency
+                            HStack{
+                                //Currency image
+                                //Currency text
+                            }
+                        //Text field
+                        }
+                        //Equal Sign
+                        //Right conversion section
+                        VStack{
+                            //Currency
+                            HStack{
+                                //Currency text
+                                //Currency image
+                            }
+                            //Text field
+                        }
+                    }
+                    //Info Button
+                }
             }
         }
-        .padding()
     }
 }
 
