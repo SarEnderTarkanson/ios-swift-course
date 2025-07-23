98fdff713b56fc903a1d78f9be9addd8d6b41b5f
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-23 18:20:35 +0200

    laying out our exchange info screen

---
+++ LOTRConverter/LOTRConverter/ExchangeInfo.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeInfo.swift
+//  LOTRConverter
+//
+//  Created by Alpy on 23/07/2025.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
