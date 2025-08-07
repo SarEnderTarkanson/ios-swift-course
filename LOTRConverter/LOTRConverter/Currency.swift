0d90e04d61b1ce76d465910283336e09ec036c52
Alparslan Tarkan <atarkan@hotmail.com>
2025-08-07 18:15:58 +0200

    models & enums

---
+++ LOTRConverter/LOTRConverter/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Alpy on 07/08/2025.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
