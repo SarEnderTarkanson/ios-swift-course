452d7d25f65211c0a09b8cd48198469a537ac56c
Alparslan Tarkan <atarkan@hotmail.com>
2026-01-15 16:55:34 +0100

    initial commit for the recap

---
+++ LOTRConverter/LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Alpy on 15/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
